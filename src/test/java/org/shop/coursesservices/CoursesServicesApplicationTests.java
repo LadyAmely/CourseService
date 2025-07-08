46981cfb9f5689c11d7089d2b8062cac56d419bd
Amelia <nawrotamelia3@gmail.com>
2025-07-08 22:27:50 +0200

    B1 - configure environment

---
+++ src/test/java/org/shop/coursesservices/CoursesServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shop.coursesservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
